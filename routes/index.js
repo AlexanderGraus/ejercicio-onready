9d3a9795e01c27260b9ce9617eb700127ed37de8
Alexander Graus <alegraus31@gmail.com>
2020-08-07 03:07:44 -0300

    ordenado el array de autos con el prototype sort()

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+let vehiculos = [];
+
 class Vehiculo{
   constructor(marca,modelo,precio){
     this.marca = marca;
@@ -11,31 +13,44 @@ class Vehiculo{
 
 class Auto extends Vehiculo{
   constructor(marca,modelo,puertas,precio){
-    super.marca = marca;
-    super.modelo = modelo;
+    super(marca,modelo,precio);
     this.puertas = puertas;
-    super.precio = precio;
+    
   }
   imprimir(){
-    return `Marca: ${this.marca} // Modelo: ${this.modelo} // Puertas: ${this.puertas} // Precio: ${this.precio}`
+    return `Marca: ${this.marca} // Modelo: ${this.modelo} // Puertas: ${this.puertas} // Precio: $${this.precio}`
   }
 }
 
 class Moto extends Vehiculo{
   constructor(marca,modelo,cilindrada,precio){
-    super.marca = marca;
-    super.modelo = modelo;
+    super(marca,modelo,precio);
     this.cilindrada = cilindrada;
-    super.precio = precio;
+    
   }
   imprimir(){
     return `Marca: ${this.marca} // Modelo: ${this.modelo} // Cilindrada: ${this.cilindrada}c // Precio: $${this.precio}`
   }
 }
+
+cargarAutos = () =>{
+  vehiculos.push(new Auto('Peugeot','206',4,200000));
+  vehiculos.push(new Moto('Honda','Titan',125,60000));
+  vehiculos.push(new Auto('Peugeot','208',5,250000));
+  vehiculos.push(new Moto('Yamaha','YBR',160,80500.50));
+}
+
+ordenarAutos = () =>{
+  vehiculos.sort((auto1,auto2)=>{
+    return auto2.precio - auto1.precio;
+  });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  
+  cargarAutos();
+  ordenarAutos();
+  console.log(vehiculos);
   res.end();
 });
 
